ac17bc93f27866d2887f888b77351bbd507b3d37
Artem451 <75530269+Artem451@users.noreply.github.com>
2021-07-26 20:16:20 +0300

    timer

--- timer.js
+++ timer.js
@@ -1,24 +1,30 @@
-const timer = {
-    
-}
-
-function initTimer(id, time) {
-  const timerSelector = document.querySelector('#timer-1')
-  const dayValue = document.querySelector('[data-value="days"]')
-  const hourValue = document.querySelector('[data-value="hours"]')
-  const minsValue = document.querySelector('[data-value="mins"]')
-  const secValue = document.querySelector('[data-value="secs"]')
-
-  function timerUpdate() {
-    const timerObj = timerComponents(time)
-
-    dayValue.innerHTML = timerObj.daysdays
-    hourValue.innerHTML = timerObj.hours
-    minsValue.innerHTML = timerObj.mins
-    secValue.innerHTML = timerObj.secs
+class CountdownTimer  {
+  constructor ({selector, targetDate}){
+    this.selector = selector
+    this.targetDate = targetDate
   }
+  get timer () {
 
+    const statrTimer = this.targetDate
+    const dayValue = document.querySelector('[data-value="days"]')
+    const hourValue = document.querySelector('[data-value="hours"]')
+    const minsValue = document.querySelector('[data-value="mins"]')
+    const secValue = document.querySelector('[data-value="secs"]')
+     setInterval (() => {
+        const curentTime = Date.now()
+        const deltaTime = statrTimer - curentTime
+        const timerComp = timerComponents(deltaTime)
+
+        dayValue.innerHTML = timerComp.days
+        hourValue.innerHTML = ('0' + timerComp.hours).slice(-2)
+        minsValue.innerHTML = ('0' + timerComp.mins).slice(-2)
+        secValue.innerHTML = ('0' + timerComp.secs).slice(-2)
+
+        return console.log(timerComp)
+      },1000) 
+    }
 }
+
 
 function timerComponents(time) {
     const days = Math.floor(time / (1000 * 60 * 60 * 24));
@@ -32,8 +38,8 @@ function timerComponents(time) {
     return {days, hours, mins, secs};
 }
 
-new CountdownTimer({
+let time = new CountdownTimer({
     selector: '#timer-1',
     targetDate: new Date('Aug 19, 2021'),
   });
-
+time.timer()
